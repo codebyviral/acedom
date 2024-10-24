5fd4e8f5df298003649a1e30a59e4a1b6fb41dcb
divyeshmoraniya <hdivyesh62@gmail.com>
2024-10-24 10:41:16 +0530

    update - vite.config.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import {React ,useEffect}from 'react'
+import Nav from './component/Nav'
+import Body from './component/Body'
+import Whitebody from './component/Whitebody'
+import { useState } from 'react'
+
+
+export default function App() {
+  const [mode, setmode] = useState('light')
+  const togglemode = () => {
+    if (mode == 'light') {
+      setmode('dark')
+      document.body.style.backgroundColor = 'dark'
+      document.body.style.color = 'white'
+    }
+    else {
+      setmode('light')
+      document.body.style.backgroundColor = 'light'
+      document.body.style.color = 'black'
+    }
+
+  }
+  return (
+    <div>
+      <Nav/>
+      <Body/>
+    </div>
+  )
+}
