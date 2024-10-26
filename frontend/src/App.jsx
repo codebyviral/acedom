9579867c790a1873fe134c7206101af8eef3b907
Viral Vaghela <viralvaghela8577@gmail.com>
2024-10-26 15:30:20 +0530

    add - test frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,28 @@
-import {React ,useEffect}from 'react'
-import Nav from './component/Nav'
-import Body from './component/Body'
-import Whitebody from './component/Whitebody'
-import { useState } from 'react'
-
+import { React, useEffect } from "react";
+import Nav from "./component/Nav";
+import Body from "./component/Body";
+import Whitebody from "./component/Whitebody";
+import { useState } from "react";
 
 export default function App() {
-  const [mode, setmode] = useState('light')
+  const [mode, setmode] = useState("light");
+  const [divyesh, setDivyesh] = useState("Ame Leri Lala");
   const togglemode = () => {
-    if (mode == 'light') {
-      setmode('dark')
-      document.body.style.backgroundColor = 'dark'
-      document.body.style.color = 'white'
+    if (mode == "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "dark";
+      document.body.style.color = "white";
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "light";
+      document.body.style.color = "black";
     }
-    else {
-      setmode('light')
-      document.body.style.backgroundColor = 'light'
-      document.body.style.color = 'black'
-    }
-
-  }
+  };
   // github ok
   return (
     <div>
-      <Nav/>
-      <Body/>
+      <Nav />
+      <Body />
     </div>
-  )
+  );
 }
