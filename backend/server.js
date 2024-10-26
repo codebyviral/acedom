efaf18380e0ccbcf33a6be11862879c7482baa71
Viral Vaghela <viralvaghela8577@gmail.com>
2024-10-26 21:11:33 +0530

    add - database connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('Acedom backend ~~~')
+import express from "express";
+import { connectToDatabase } from "./db/db.js";
+
+const app = express();
+
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    console.log(`This is Acedom's Backend ⚔️`);
+    res.send("Welcome to Acedom's Backend!");
+})
+
+connectToDatabase().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port} 🛜`);
+    })
+})
