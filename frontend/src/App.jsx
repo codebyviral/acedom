8e86e0befd89fa90440f56f0ed14f1afd8612089
divyeshmoraniya <hdivyesh62@gmail.com>
2024-10-26 16:48:32 +0530

    set code efficiant

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
-import Nav from './component/Nav';
-import Body from './component/Body';
+import { React, useEffect } from "react";
+import Nav from "./component/Nav";
+import Body from "./component/Body";
+import { useState } from "react";
 
 export default function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setmode] = useState("light");
+  const [divyesh, setDivyesh] = useState("Ame Leri Lala");
 
   const toggleMode = () => {
     if (mode === 'light') {
-      setMode('dark');
+      setmode('dark');
       document.body.style.backgroundColor = 'black';
       document.body.style.color = 'white';
     } else {
-      setMode('light');
+      setmode('light');
       document.body.style.backgroundColor = 'white';
       document.body.style.color = 'black';
     }
   };
-
+  // github ok
   return (
     <div>
       <Nav mode={mode} toggleMode={toggleMode} />
-      <Body mode={mode} />
+      <Body mode={mode}  />
     </div>
   );
 }
